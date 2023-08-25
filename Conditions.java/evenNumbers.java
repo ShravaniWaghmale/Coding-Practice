badfa9fb292c8b42960b8ea47d366125c875ab55
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-08-25 17:04:32 +0530

    adding commit

---
+++ Conditions.java/evenNumbers.java
@@ -0,0 +1,19 @@
+package Conditions;
+
+import java.util.Scanner;
+
+public class evenNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+       
