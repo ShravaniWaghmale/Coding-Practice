6c8347b15005cb9145ad83931c77d3237a8a6e0d
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-08-29 16:48:20 +0530

    adding commit

---
+++ Conditions.java/operator.java
@@ -0,0 +1,34 @@
+package Conditions;
+
+import java.util.*;
+
+public class operator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int operator = sc. nextInt();
+
+        switch(operator) {
+            case 1 : System.out.println(a + b);
+            break;
+            case 2 : System.out.println(a - b);
+            break;
+            case 3 : System.out.println(a * b);
+            break;
+            case 4 : if (b == 0) {
+                System.out.println("invalid division");
+                } else {
+                    System.out.println(a/b);
+                }
+                break;
+                case 5 : if (b == 0) {
+                    System.out.println("invalid division");
+                    } else {
+                        System.out.println(a%b);
+                    }
+                    break;
+                    default : System.out.println("invalid Operator");
+        }
+    }
+}
