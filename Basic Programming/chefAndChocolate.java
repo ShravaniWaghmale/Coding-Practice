ea3ef26b8998a9184146b78896a71e4714e9d08c
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-08-30 12:50:43 +0530

    Create chefAndChocolate.java

---
+++ Basic Programming/chefAndChocolate.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-->0) {		
+			int x = sc.nextInt()*5;
+			int y = sc.nextInt()*10;
+			int z = sc.nextInt();
+			
+			int a = x + y;
+			int b = a/z;
+			System.out.println(b);
+		}
+	}
+}
