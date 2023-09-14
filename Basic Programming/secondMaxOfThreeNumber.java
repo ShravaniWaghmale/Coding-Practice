c7365f171248b928d39022be6b44032fe5bfbfbc
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-09-14 19:30:46 +0530

    New Problem

---
+++ Basic Programming/secondMaxOfThreeNumber.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+		    int n1 = sc.nextInt();
+		    int n2 = sc.nextInt();
+		    int n3 = sc.nextInt();
+		    int arr[] = new int[]{n1,n2,n3};
+		    Arrays.sort(arr);
+		    System.out.println(arr[1]);
+		}    
+	}
+}
