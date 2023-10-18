c3d68271d54ea4e97c054ee823b96569aa9c5da1
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-10-18 18:48:38 +0530

    New Problem

---
+++ Basic Programming/interiorDesign.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+		    int x1 = sc.nextInt();
+		    int y1 = sc.nextInt();
+		    int x2 = sc.nextInt();
+		    int y2 = sc.nextInt();
+		    if(x1+y1 > x2+y2) {
+		        System.out.println(x2+y2);
+		    } else {
+		        System.out.println(x1+y1);
+		    }
+		}
+	}
+}
