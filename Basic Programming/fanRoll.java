bd84ba9412142a460060b2956c17b2fc9cd16536
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-11-01 20:58:51 +0530

    New Problem

---
+++ Basic Programming/fanRoll.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a>b && a>c) {
+		    System.out.println("yes");
+		} else if (b>a && b>c) {
+		    System.out.println("no");
+		} else {
+		    System.out.println("no");
+		}
+	}
+}
