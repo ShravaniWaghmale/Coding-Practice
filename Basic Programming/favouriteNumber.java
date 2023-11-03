6f9ebefd718d8cf7d62e97af77a7ab8b3f6fe49a
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-11-03 19:37:37 +0530

    New Problem

---
+++ Basic Programming/favouriteNumber.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt(); 
+		for(int i=0;i<t;i++) {
+		   
+		   int a = sc.nextInt();
+		   System.out.println((a%2==0 && a%7 == 0)?"Alice":(a%2!=0 && a%9 == 0)?"Bob":"Charlie");
+		}
+	}
+}
