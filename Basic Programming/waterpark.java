19df737c591fa93e9f3f5c287f3446ff608aabcf
Shravani Waghmale <140416879+ShravaniWaghmale@users.noreply.github.com>
2023-11-30 21:40:38 +0530

    New Problem

---
+++ Basic Programming/waterpark.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		if(60<=w && 130>=h) {
+		    System.out.println("Yes");
+		} else {
+		    System.out.println("No");
+		}
+	}
+}
